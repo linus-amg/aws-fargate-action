cd29938407d25814d52e850ae115764586d4a287
LG <7453396+linus-amg@users.noreply.github.com>
2020-04-26 17:53:35 +0200

    remove master only

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,6 @@
 set -e
 set -o pipefail
 
-if [[ "$GITHUB_REF" != "refs/heads/master" ]]; then
-	echo "$GITHUB_REF was not master, exiting..."
-	exit 0
-fi
-
 echo "On branch ${GITHUB_REF}, deploying..."
 
 (
